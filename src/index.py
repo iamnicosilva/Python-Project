38aeb7cb8bb445541ac2aa170359c6930478bcee
P3L4 <silvanicolas93@gmail.com>
2022-07-19 04:01:01 -0300

    aguegue comentario de version en index.py

--- src/index.py
+++ src/index.py
@@ -1,3 +1,5 @@
+# version beta 1.0 con error de acceso al retornar a main
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -12,4 +14,3 @@ def about():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
